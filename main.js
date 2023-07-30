1d2cd70544ce8eb093815a04c6402c3c9ce6d091
mohamed53-53 <67844776+mohamed53-53@users.noreply.github.com>
2023-07-29 19:10:15 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+const apiKey = "fc1036bf842d2e543f7bc6d62a710b4b"
+var input = document.querySelector(".search input")
+var button = document.querySelector(".search button")
+var temp = document.querySelector(".states .temp")
+var humidity = document.querySelector(".humidity")
+var wind = document.querySelector(".wind")
+var city = document.querySelector(".city")
+var weathre_icon = document.querySelector(".states img")
+var states = document.querySelector(".states")
+var details = document.querySelector(".details")
+
+
+async function cheackWeather(apiUrl) {
+    const response = await fetch(apiUrl + `&appid=${apiKey}`)
+    var data = await response.json();
+    if (data['cod'] === 200) {
+        temp.innerText = ""
+        humidity.innerText = ""
+        wind.innerText = ""
+        city.innerText = ""
+        weathre_icon.src = ""
+        temp.innerText = (data['main']['temp'] - 273.15).toFixed(0) + "°c"
+        humidity.innerText = data['main']['humidity'] + "%"
+        wind.innerText = data['wind']['speed'] + "km/h"
+        city.innerText = data['name']
+        console.log(data['weather']['0']['main'])
+        weathre_icon.src = `/images/${data['weather']['0']['main']}.png`
+        states.style = 'display:block'
+        details.style = 'display:flex'
+    }
+
+    
+}
+button.addEventListener("click", () => {
+    if (input.value !== '') {
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}`
+        cheackWeather(apiUrl)
+    }
+})
+
